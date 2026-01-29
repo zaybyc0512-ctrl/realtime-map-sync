13530cd0172d230cd741c3e483e145d55c3d409a
zaybyc0512-ctrl <zaybyc0512@gmail.com>
2026-01-30 08:48:22 +0900

    ko-fi

--- src/components/ui/SocialLinks.tsx
+++ src/components/ui/SocialLinks.tsx
@@ -22,6 +22,21 @@ const SOCIAL_LINKS = [
 export const SocialLinks = () => {
     return (
         <div className="fixed bottom-4 right-4 z-50 flex items-center space-x-3 rounded-lg bg-white/90 p-2 shadow-md backdrop-blur-sm transition-opacity hover:opacity-100 opacity-80">
+            {/* Ko-fi Button */}
+            <a
+                href='https://ko-fi.com/W7W71T8OVD'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:opacity-80 transition-opacity mr-2 flex items-center'
+            >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    height='36'
+                    style={{ border: '0px', height: '36px' }}
+                    src='https://storage.ko-fi.com/cdn/kofi6.png?v=6'
+                    alt='Buy Me a Coffee at ko-fi.com'
+                />
+            </a>
             {SOCIAL_LINKS.map((link) => (
                 <a
                     key={link.name}
